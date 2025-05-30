43f072e533a7d7f597f1b63ff3ede93d00a0f68c
Balamurugan/DCKAP <balamuruganr@dckap.com>
2025-05-30 12:58:25 +0530

    Test 02 initial commit

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication1.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(); // Index 02
         }
 
         public IActionResult Privacy()
